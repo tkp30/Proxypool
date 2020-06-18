d4d58b629d84ca18b710689d55108443cbf2aac1
XHG78999 <53939187+XHG78999@users.noreply.github.com>
2020-06-18 19:55:20 +0800

    Inital commit

--- database.py
+++ database.py
@@ -1,78 +1,47 @@
-import sqlite3
-import time
-import os
-from config import config
+from pymongo import MongoClient
 import requests
+import time
 class DB(object):
     def __init__(self,path=config["db"]["file"]):
         self.initalize(path)
     def initalize(self,path):
-        '''初始化函数'''
-        table=False
-        if config["db"]["refresh"]:
-            os.remove(config["db"]["file"])
-        if not os.path.exists(config["db"]["file"]):
-            table=True
-        self.db=sqlite3.connect(path,check_same_thread = False)
-        self.cu=self.db.cursor()
-        if table:
-            self.cu.execute('''CREATE TABLE PROXY
-            (IP TEXT NOT NULL,
-            UPTIME TEXT NOT NULL,
-            CONTURY TEXT NOT NULL,
-            OPERATORS TEXT NOT NULL);''')
-            self.db.commit()
+        client=pymongo.MongoClient(path)
+        if config["db"]["refresh"] and "proxypool" in client.list_database_names():
+            client.dropDatabase(client.proxypool)
+        self.db=client["proxypool"]["proxy"]
     def add(self,proxy):
-        '''非阻塞加入代理'''
         for x in proxy:
             self.__add(x)
     def __add(self,proxy,*arg):
-        '''加入代理底层接口'''
         t=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
         c=self.getcontury(proxy)
-        if not config["db"]["update"]:
-            if proxy not in self.getall():
-                self.db.execute('''INSERT INTO PROXY (IP,UPTIME,CONTURY,OPERATORS) \
-            VALUES (?,?,?,?);''',(proxy,t,c[0],c[1]))
-            else:
-                self.db.execute('''UPDATE PROXY SET UPTIME=? WHERE IP=?;''',(t,proxy))
-        else:
-            self.db.execute('''INSERT INTO PROXY (IP,UPTIME,CONTURY,OPERATORS) \
-        VALUES (?,?,?,?);''',(proxy,t,c[0],c[1]))
-        try:
-            self.db.commit()
-        except:
-            pass
+        self.db.insert({
+            "IP":proxy,
+            "UPTIME":t,
+            "CONTURY":c[0],
+            "OPERATORS":c[1]
+        })
     def remove(self,proxy):
-        '''删除代理'''
-        self.cu.execute("DELETE from PROXY where IP=?;",(proxy))
-        self.db.commit()
+        self.db.remove({"IP":proxy})
     def getall(self):
-        '''获取全部代理'''
-        cursor = [x[0] for x in list(filter(None,list(self.cu.execute("SELECT IP,UPTIME,CONTURY,OPERATORS from PROXY"))))]
-        self.db.commit()
-        return cursor
+        return [x["IP"] for x in self.get()]
     def get(self):
-        '''获取全部代理及数据'''
-        cursor = list(filter(None,list(self.cu.execute("SELECT IP,UPTIME,CONTURY,OPERATORS from PROXY"))))
-        self.db.commit()
-        return cursor
+        return list(self.db.find())
     def gettime(self,proxy):
-        '''获取某个代理的更新时间'''
-        cursor = [x[1] for x in list(filter(None,list(self.cu.execute("SELECT IP,UPTIME,CONTURY,OPERATORS from PROXY")))) if x[0]==proxy]
-        self.db.commit()
-        return cursor
+        return self.db.find({"IP":proxy})[0]["UPTIME"]
+    def getdbcontury(self,proxy):
+        return self.db.find({"IP":proxy})[0]["CONTURY"]
+    def getinfo(self,proxy):
+        s=self.db.find({"IP":proxy})[0]
+        return {
+            "ip":proxy,
+            "uptime":s["UPTIME"],
+            "contury":s["CONTURY"],
+            "operators":s["OPERATORS"]
+        }
     def getcontury(self,proxy):
-        '''IP定位'''
         r=requests.get("http://whois.pconline.com.cn/ipJson.jsp?ip=%s&json=true"%proxy)
         if r.json()["err"]!="":
             return ["未知"]*2
-        return r.json()["addr"].split(" ")
-    def getdbcontury(self,proxy):
-        '''获取已有IP的地区'''
-        cursor = [x[2] for x in self.get() if x[0]==proxy]
-        self.db.commit()
-        return cursor
-    def __del__(self):
-        '''销毁方法'''
-        self.db.close()
+        return [r.json()["addr"].split(" ")[0],r.json()["addr"].split(" ")[1].strip("ADSL")]
+        
